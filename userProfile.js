446b26e935c34fb7adf0d239cae3dc373d4f03f2
Capybara16 <laurentinokris16@gmail.com>
2024-11-04 17:56:55 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+// Declare variables to store user information
+let fullName = "Kristine Duga Laurentino";  
+let age = 19;               
+let favoriteNumber = 16;     
+let favoriteColor = "black"; 
+
+// Log the information to the console
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
